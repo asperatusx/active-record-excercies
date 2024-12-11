c39fe6512bf9e155aa81b3eb27b0e3f6b1bec553
Andrew <andrewlang.p@gmail.com>
2024-12-10 21:45:24 -0500

    use active record calculations like sum, avg, and chaining

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+totalRevenue = Store.sum(:annual_revenue)
+puts "total revenue of all stores: #{totalRevenue} "
+
+avgRevenue = Store.average(:annual_revenue)
+puts "avg revenue of all stores: #{avgRevenue}"
+
+storeCount = Store.where(['annual_revenue >= ?', 1000000]).count
+puts "There are #{storeCount} stores that have 1m or more in annual sales"
