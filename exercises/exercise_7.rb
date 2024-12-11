ae7948cf7c54e3cbf67e9a3c23b0a4e22025a306
Andrew <andrewlang.p@gmail.com>
2024-12-11 14:55:17 -0500

    add validations and check if validations work

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,16 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Please enter a store name:"
+store_name = gets.chomp
+
+store = Store.create(name: store_name)
+
+if store.errors.any?
+  puts "Error(s) occurred while creating the store:"
+  store.errors.full_messages.each do |message|
+    puts "- #{message}"
+  end
+else
+  puts "Store '#{store_name}' was created successfully!"
+end
